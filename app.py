9b2ff088263080067d4759e1b5ba50a37d783328
Antoni Casas <tonicasas6@gmail.com>
2020-06-10 19:18:31 +0200

    Adds requirements.txt and implementation of extractor with API

--- app.py
+++ app.py
@@ -1,49 +1,78 @@
 import math
 from collections import Counter
 
-from flask import Flask, jsonify
-import numpy as np
-from scipy.optimize import minimize
-import spacy
 import neuralcoref
 import nltk
+import spacy
+from flask import Flask, request
+from flask_restx import Api, Resource, fields
 
 app = Flask(__name__)
+api = Api(app, version='1.0', title='Text Metrics API',
+          description='A text metric extractor',
+          )
+
+ns = api.namespace('Metrics', description='Text metrics')
+
+textsModel = api.model('Text',
+                      {
+    'id': fields.Integer(readOnly=True, description='The text identifier'),
+    'text': fields.String(required=True, description='The text to analyze')
+})
 
 
-@app.route('/allMetrics', methods=['POST'])
-def allMetrics(texts):
-    zipf = getZipf(texts)
-    corref = getCorreferences(texts)
-    wordLength = getWordLength(texts)
-    word2vec = getWord2VecContinuity(texts)
-    conditionalEntropy = getConditionalEntropy(texts)
-    conditionalEntropyPOS = getConditionalEntropyPOS(texts)
-    numSynSets = getNumSynSets(texts)
-    posDistribution = getPOSDistribution(texts)
-    return jsonify()
 
+@ns.route('/allMetrics')
+class Metrics(Resource):
 
-def getZipf(texts):
-    zipfVals = []
-    for text in texts:
-        c = Counter(text)
+    @ns.doc('Obtain metrics')
+    #@ns.marshal_with(metricsModel,as_list=True)
+    @ns.expect([textsModel])
+    @ns.doc(params={'payload': 'Expected array of texts, defined by their ID, returns a map with each quality '})
+    def post(self):
+        toRet = []
+        data = request.json
+        for text in data:
+            corref = self.getCorreferences(text["text"])
+            numSynSets = self.getNumSynSets(text["text"])
+            posDistribution = self.getPOSDistribution(text["text"])
+            auxText=text["text"].replace(',', '' '').replace('.', ' ').replace(':', " ").replace('-', ' ')
+            zipf = self.getZipf(auxText)
+            wordLength = self.getWordLength(auxText)
+            conditionalEntropy = self.getConditionalEntropy(auxText)
+            conditionalEntropyPOS = self.getConditionalEntropyPOS(auxText)
+            conditionalEntropyChar = self.getConditionalEntropyChar(auxText)
+            metricObject = {"zipf": zipf,
+                            "corref": corref,
+                            "wordLength": wordLength,
+                            "conditionalEntropy": conditionalEntropy,
+                            "conditionalEntropyPOS": conditionalEntropyPOS,
+                            "numSynSets": numSynSets,
+                            "posDistribution": posDistribution,
+                            "conditionalEntropyChar": conditionalEntropyChar,
+                            }
+            returnObject= {
+                "metrics":metricObject,
+                "id": text["id"]
+
+            }
+            toRet.append(returnObject)
+        return toRet
+
+    x = None
+    s = None
+
+    def getZipf(self, text):
+        c = Counter(text.split())
         currentArr = []
         for val in c:
             currentArr.append(c[val])
-            x = np.arange(1, len(currentArr) + 1, 1)
-            s = np.array(currentArr).astype(int)
-            s[::-1].sort()
-            s_best = minimize(loglik(x=x), [2])
-            zipfVals.append(s_best.x[0])
-    return zipfVals
+        currentArr.sort(reverse=True)
+        return currentArr
 
-
-def getCorreferences(texts):
-    nlp = spacy.load('en_core_web_sm')
-    neuralcoref.add_to_pipe(nlp)
-    correfs = []
-    for text in texts:
+    def getCorreferences(self, text):
+        nlp = spacy.load('en_core_web_sm')
+        neuralcoref.add_to_pipe(nlp)
         corref = {}
         doc = nlp(text)
         for item in doc._.coref_clusters:
@@ -52,54 +81,53 @@ def getCorreferences(texts):
                 corref[size] += 1
             else:
                 corref[size] = 1
-        correfs.append(corref)
-    return correfs
+        toNormalizeBy=sum(corref.values())
+        if toNormalizeBy==0:
+            factor=1
+        else:
+            factor = 1.0 / toNormalizeBy
+        for item in corref:
+            corref[item]=corref[item]*factor
+        return corref
 
-
-def getWordLength(texts):
-    wordLength=[]
-    for text in texts:
-        wordLen={}
+    def getWordLength(self, text):
+        wordLen = {}
         for word in text.split():
-            length=len(word)
+            length = len(word)
             if length in wordLen:
-                wordLen[length]+= 1
+                wordLen[length] += 1
             else:
-                wordLen[length]=1
-        wordLength.append(wordLen)
-    return wordLength
+                wordLen[length] = 1
+        toNormalizeBy=sum(wordLen.values())
+        if toNormalizeBy==0:
+            factor=1
+        else:
+            factor = 1.0 / toNormalizeBy
+        for item in wordLen:
+            wordLen[item]=wordLen[item]*factor
+        return wordLen
 
+    def getConditionalEntropyChar(self, text):
+        return self.computeCompleteHillbergConditional(list(text), 14,True)
 
-def getConditionalEntropy(texts):
-    conditionalEntropy=[]
-    for text in texts:
-        conditionalEntropy.append(computeCompleteHillbergConditional(text, 8))
+    def getConditionalEntropy(self, text):
+        return self.computeCompleteHillbergConditional(text, 12)
 
+    def getConditionalEntropyPOS(self,text):
+        POStext = self.turnToPOS( text)
+        return self.computeCompleteHillbergConditional(POStext, 8)
 
-def getConditionalEntropyPOS(texts):
-    conditionalEntropy=[]
-    for text in texts:
-        POStext=turnToPOS(text)
-        conditionalEntropy.append(computeCompleteHillbergConditional(POStext, 8))
+    def turnToPOS(self, text):
+        posText = ""
+        for sentence in text.split("\n"):
+            tokens = nltk.word_tokenize(sentence)
+            postag = nltk.pos_tag(tokens)
+            for tag in postag:
+                posText += " " + str(tag[1])
+        return posText
 
-def turnToPOS(text):
-    posText=""
-    for sentence in text.split("\n"):
-        tokens = nltk.word_tokenize(sentence)
-        postag = nltk.pos_tag(tokens)
-        for tag in postag:
-            posText+=" "+str(tag[1])
-    return posText
-
-
-def getWord2VecContinuity(texts):
-    return None
-
-
-def getNumSynSets(texts):
-    nlp = spacy.load("en_core_web_sm")
-    synsets = []
-    for text in texts:
+    def getNumSynSets(self, text):
+        nlp = spacy.load("en_core_web_sm")
         synset = {}
         doc = nlp(text)
         for token in doc:
@@ -108,13 +136,16 @@ def getNumSynSets(texts):
                 synset[distance] += 1
             else:
                 synset[distance] = 1
-        synsets.append(synset)
-    return synsets
+        toNormalizeBy=sum(synset.values())
+        if toNormalizeBy==0:
+            factor=1
+        else:
+            factor = 1.0 / toNormalizeBy
+        for item in synset:
+            synset[item]=synset[item]*factor
+        return synset
 
-
-def getPOSDistribution(texts):
-    distributions = []
-    for text in texts:
+    def getPOSDistribution(self, text):
         tagAux = {}
         for sentence in text.split("\n"):
             tokens = nltk.word_tokenize(sentence)
@@ -130,92 +161,75 @@ def getPOSDistribution(texts):
         factor = 1.0 / sumVal
         for k in tagAux:
             tagAux[k] = tagAux[k] * factor
-        distributions.append(tagAux)
-    return distributions
+        return tagAux
 
 
-def loglik(x, b):
-    # Power law function
-    Probabilities = x ** (-b)
-
-    # Normalized
-    Probabilities = Probabilities / Probabilities.sum()
-
-    # Log Likelihoood
-    Lvector = np.log(Probabilities)
-
-    # Multiply the vector by frequencies
-    Lvector = np.log(Probabilities) * s
-
-    # LL is the sum
-    L = Lvector.sum()
-
-    # We want to maximize LogLikelihood or minimize (-1)*LogLikelihood
-    return (-L)
-
-
-def computeWindowsConditional(text,windowSize):
-    frequenciesNgram={}
-    conditionalFrequencies={}
-
-    words=text.split()
-    for j in range(0,len(words)-windowSize):
-        ngram=''
-        first=True
-        for t in range(j,j+windowSize):
-            word=words[t]
-            if first:
-                ngram=word
-                first=False
-            else:
-                    ngram=ngram+"_"+word
-        if not ngram in frequenciesNgram:
-                frequenciesNgram[ngram]=1
+    def computeWindowsConditional(self, text, windowSize):
+        frequenciesNgram = {}
+        conditionalFrequencies = {}
+        if not list:
+            words = text.split()
         else:
-                frequenciesNgram[ngram]=frequenciesNgram[ngram]+1
-    for j in range(0,len(words)-windowSize):
-        ngram=''
-        first=True
-        for t in range(j,j+windowSize):
-            word=words[t]
-            if first:
-                ngram=word
-                first=False
+            words=text
+        for j in range(0, len(words) - windowSize):
+            ngram = ''
+            first = True
+            for t in range(j, j + windowSize):
+                word = words[t]
+                if first:
+                    ngram = word
+                    first = False
+                else:
+                    ngram = ngram + "_" + word
+            if not ngram in frequenciesNgram:
+                frequenciesNgram[ngram] = 1
             else:
-                ngram=ngram+"_"+word
-        if not ngram in conditionalFrequencies:
-            conditionalFrequencies[ngram]={}
-            conditionalFrequencies[ngram][words[j+windowSize]]=1
-        else:
-            if words[j+windowSize] in conditionalFrequencies[ngram]:
-                conditionalFrequencies[ngram][words[j+windowSize]]=conditionalFrequencies[ngram][words[j+windowSize]]+1
+                frequenciesNgram[ngram] = frequenciesNgram[ngram] + 1
+            if not ngram in conditionalFrequencies:
+                conditionalFrequencies[ngram] = {}
+                conditionalFrequencies[ngram][words[j + windowSize]] = 1
             else:
-                conditionalFrequencies[ngram][words[j+windowSize]]=1
-    return frequenciesNgram,conditionalFrequencies
+                if words[j + windowSize] in conditionalFrequencies[ngram]:
+                    conditionalFrequencies[ngram][words[j + windowSize]] = conditionalFrequencies[ngram][
+                                                                               words[j + windowSize]] + 1
+                else:
+                    conditionalFrequencies[ngram][words[j + windowSize]] = 1
 
+        #for j in range(0, len(words) - windowSize):
+            # ngram = ''
+            #first = True
+            #for t in range(j, j + windowSize):
+                #   word = words[t]
+                #if first:
+                    #    ngram = word
+                    #first = False
+                    #else:
+        # ngram = ngram + "_" + word
+        return frequenciesNgram, conditionalFrequencies
 
-def computeHillbergLawConditional(windowsy,windowsxy):
-    n=sum(windowsy.values())
-    tot=n
-    sumEntropy=0
-    if len(windowsy)>=1 and tot>=1:
-        for l in windowsy.keys():
-            px=windowsy[l]/tot
-            if l in windowsxy:
-                windowxy=windowsxy[l]
-                for key in windowxy.keys():
-                    pxy=windowxy[key]/tot
-                    logp=-math.log2(pxy/px)
-                    logp=logp*pxy
-                    sumEntropy=sumEntropy+logp
-    return sumEntropy
+    def computeHillbergLawConditional(self, windowsy, windowsxy, list=False):
+        n = sum(windowsy.values())
+        tot = n
+        sumEntropy = 0
+        if len(windowsy) >= 1 and tot >= 1:
+            for l in windowsy.keys():
+                px = windowsy[l] / tot
+                if l in windowsxy:
+                    windowxy = windowsxy[l]
+                    for key in windowxy.keys():
+                        pxy = windowxy[key] / tot
+                        logp = -math.log2(pxy / px)
+                        logp = logp * pxy
+                        sumEntropy = sumEntropy + logp
+        return sumEntropy
 
-def computeCompleteHillbergConditional(text,windowSize):
-    hillbergVals=[]
-    for i in range(1,windowSize):
-        windowsy,windowsxy=computeWindowsConditional(text,i)
-        hillbergVals.append(computeHillbergLawConditional(windowsy,windowsxy,i))
-    return hillbergVals
+    def computeCompleteHillbergConditional(self, text, windowSize, list=False):
+        hillbergVals = []
+        for i in range(1, windowSize):
+            windowsy, windowsxy = self.computeWindowsConditional(text, i)
+            hillbergVals.append(self.computeHillbergLawConditional(windowsy, windowsxy,list))
+        return hillbergVals
+
 
 if __name__ == '__main__':
     app.run()
